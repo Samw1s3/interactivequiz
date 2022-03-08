931a20f7485d9af1722ded2c4785f8e5a8343e30
Sam Knopper <samanthaknopper@hotmail.com>
2022-03-08 11:39:38 +0800

    app loop created, local storage not working and questions not changing

--- assests/questions.js
+++ assests/questions.js
@@ -6,47 +6,126 @@
 
 const questions = [
     {
-        title: "What are you able do to with JavaScript",
+        title: "JavaScript handles web page interaction",
         choices: [
             {
-                title: "1",
+                title: "True",
                 isAns: true,
             },
             {
-                title: "2",
+                title: "False",
+                isAns: false,
+            },
+        ],
+
+    },
+    { 
+        title: "If I use the keyword 'var'. What am I creating?",
+        choices: [
+            {
+                title: "Variable",
+                isAns: true,
+            },
+            {
+                title: "Variety",
                 isAns: false,
             },
             {
-                title: "3",
+                title: "Various",
                 isAns: false,
             },
             {
-                title: "4",
+                title: "Variform",
                 isAns: false,
             },
         ],
 
     },
     {
-        title: "Question 2",
+        title: "What is an alert refering to in JavaScript?",
         choices: [
             {
-                title: "1",
+                title: "Asks a question",
+                isAns: false,
+            },
+            {
+                title: "Shows a message",
                 isAns: true,
             },
             {
-                title: "2",
+                title: "Makes a sound",
                 isAns: false,
             },
             {
-                title: "3",
-                isAns: false,
-            },
-            {
-                title: "4",
+                title: "Pays attention",
                 isAns: false,
             },
         ],
-
     },
+    {   
+        title: "You can check for code errors in the brower's __________?",
+        choices: [
+            {
+                title: "cache",
+                isAns: false,
+            },
+            {
+                title: "source",
+                isAns: false,
+            },
+            {
+                title: "codex",
+                isAns: false,
+            },
+            {
+                title: "console",
+                isAns: true,
+            },
+        ],
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: [
+            {
+                title: "<javascript>",
+                isAns: false,
+            },
+            {
+                title: "<js>",
+                isAns: false,
+            },
+            {
+                title: "<script>",
+                isAns: true,
+            },
+            {
+                title: "<scripting",
+                isAns: false,
+             },
+        ],
+    
+    },
+    {
+        title: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        choices: [
+            {
+                title: "if i=! 5 then",
+                isAns: false,
+            },
+            {
+                title: "if (i<>5)",
+                isAns: false,
+            },
+            {
+                title: "if i <> 5",
+                isAns: false,
+            },
+            {
+                title: "if (i != 5)",
+                isAns: true,
+             },
+        ],
+    
+    },
+    
 ];
--- assests/script.js
+++ assests/script.js
@@ -1,4 +1,4 @@
-const buttonStart = document.getElementById('button-start');
+const buttonStart = document.getElementById("button-start");
 const sectionQuestion = document.getElementById("section-questions");
 const sectionLanding = document.getElementById("section-landing");
 const sectionTimer = document.getElementById("section-timer");
@@ -7,11 +7,27 @@ const spanTime = document.getElementById("span-time");
 const questionTitle = document.getElementById("question-title");
 const spanFinalHighscore = document.getElementById("span-final-highscore");
 const questionChoices = document.getElementById("question-choices");
+const correctAnswer = document.getElementById("correct-answer");
+const wrongAnswer = document.getElementById("wrong-answer");
+const isAns = document.querySelector("questions.choices.isAns");
+const sectionHighscores = document.getElementById("highscore-rankings");
+const submitButton = document.getElementById("submit-button");
+
+const initialsInput = document.getElementById("input-initials");
+const ScoreHistoy = document.getElementById("highscore-history");
+const playAgainButton = document.getElementById("play-again-button");
+
+// const userInitials = document.getElementById("input-initials");
+
+var initials = [];
+var score = [];
 
 let timerId = null;
-let timeRemaining = 60;
+let timeRemaining = 6;
 
 let currentQuestionIndex = 0;
+let acceptingAnswers = true;
+
 
 spanTime.textContent = timeRemaining;
 
@@ -28,6 +44,7 @@ buttonStart.addEventListener('click', function (event) {
 
     showQuestion(0);
 
+    //    nextQuestion();
 })
 
 //time
@@ -51,17 +68,19 @@ function startTimer() {
 
 }
 
-function showQuestion(index){
+function showQuestion(index) {
     const question = questions[index];
 
     questionTitle.textContent = question.title;
 
+
     //loop through the choices
 
     // generate li for each
+    questionChoices.textContent = "";
 
-    for (let index = 0; index < question.choices.length; index++) {
-        const choice = question.choices[index];
+    for (var index = 0; index < question.choices.length; index++) {
+        const choice = question.choices[index]
 
         const li = document.createElement('li');
 
@@ -71,18 +90,79 @@ function showQuestion(index){
         li.appendChild(button);
 
         questionChoices.appendChild(li);
-
     }
+    questionChoices.addEventListener("click", e => {
+        console.log(e.target);
+       
+        index = index + 1
+    console.log(index);
+        // nextQuestion ();
+    
+    })
 }
+
+//function nextQuestion() {
+
+//    button.addEventListener('click', function(event){
+//      if (choice.inAns == true) {
+//        index = ++;
+
+
+//question = questions[index]
+
+//questionTitle.textContent = question.title;
+
+//questionChoices.textContent = "";
+
+//for (let index = 0; index < question.choices.length; index++) {
+//    const choice = question.choices[index]
+
+//    const li = document.createElement('li');
+
+//    const button = document.createElement('button');
+//     button.textContent = choice.title;
+
+//    li.appendChild(button);
+//
+//     questionChoices.appendChild(li);
+//  }
+
+//      })
+//} 
+//   if (choice.inAns.true) {
+//  }
+//        button.addEventListener('click', true)
+//       index = ++
+
+
+
+
+// if (choice.isAns.true) {
+
+//}
+
+//help!!
+//questionChoices.forEach(choice => {
+//  questionChoices.addEventListener('click')
+//if (inAns === true) {
+//       correctAnswer.classList.remove('hide')
+//      index++
+//  }     
+//   nextQuestion()
+
+//Questions
+
+// when Click on the choice
+// should move to the next questio
+//function nextQuestion() {
+//   index++
+//}
 
 //high score page
 //1. click on the play again btn
 //redirect the user to the landing page
 
-//Questions
 
-// when Click on the choice
-// should move to the next question
 
 // If user clicks on correct choice
 // give feedback to say it was the correct choice
@@ -108,11 +188,85 @@ function endGame() {
     //Hide question screen
     sectionQuestion.classList.add('hide')
 
-    // 3. show the high score in the end game screen
+    // 3. show the current score in the end game screen
     //hs -- time remaining
     spanFinalHighscore.textContent = timeRemaining
+
+    submitButton.addEventListener('click', saveScore())
+
+    //showHighScores();
 }
 
+//const initials = document.querySelector("input-initials")
+
+//const submitButton = document.getElementById("submit-button")
+
+//initials.addEventListener('keyup', () => {
+//    submitButton.disabled = !initials.value
+//})
+
+// Save High Scores
+
+function saveScore() {
+    var score = timeRemaining;
+    localStorage.setItem("Initials" + "Score", JSON.stringify(initials))
 
 
-//End Game Screen
+    initialsInput.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        initials.addEventListener('keyup', () => {
+            submitButton.disabled = !initials.value
+
+        }) 
+    }
+    )
+
+    submitButton.addEventListener('click', function(){
+        sectionInitials.classList.add('hide')
+    sectionHighscores.classList.remove('hide')
+    })
+}
+
+playAgainButton.addEventListener('click', function (event) {
+
+        sectionHighscores.classList.add('hide')
+
+        sectionLanding.classList.remove('hide')
+    })
+//saveHighScore = timeRemaining => {
+//    timeRemaining.preventDefault()
+
+//    const score = {
+//        score: 
+//    }
+//} 
+// end game screen 
+
+function showHighScores() {
+    // Clear todoList element and update todoCountSpan
+    sectionInitials.classList.add('hide')
+    sectionHighscores.classList.remove('hide')
+
+    var storedScores = JSON.parse(localStorage.getItem("initials" + "Score"));
+
+    if (storedScores !== null) {
+        rankings = storedScores;
+    }
+
+    ScoreHistoy.innerHTML = "";
+
+    // Render a new li for each todo
+    for (var i = 0; i < 6; i++) {
+        var rankings = rankings[i];
+
+        var li = document.createElement("li");
+        ol.textContent = initials + score;
+        ol.setAttribute("data-index", i);
+
+        ScoreHistoy.appendChild(li);
+    }
+
+    
+}
